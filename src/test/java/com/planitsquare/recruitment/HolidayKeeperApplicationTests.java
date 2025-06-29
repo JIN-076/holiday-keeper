4d37da7a9c0bd2287de93ef645bd4eee2f36d189
JIN-076 <andy230@naver.com>
2025-06-30 01:53:43 +0900

    INIT : 프로젝트 환경 세팅, 관련 의존성 추가

---
+++ src/test/java/com/planitsquare/recruitment/HolidayKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planitsquare.recruitment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayKeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
